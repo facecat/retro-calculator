adb0dc502e89f75ec5c6d10faae4d2516f7a48dd
facecat <facecat10002gmail.com>
2015-12-12 17:28:08 +0800

    Add numberBtnSound

--- retro-calculator/ViewController.swift
+++ retro-calculator/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    //MARK: Outlets
+    @IBOutlet weak var ouputLabel: UILabel!
+    
+    //MARK: Properties
+    var btnSound: AVAudioPlayer!
+    var runningNum = "" //显示在屏幕上的数字
+    
+    
+    //MARK: I don't know how to call this.
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        //Prepare for AVAudioPlayer
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        
+        let soundURL = NSURL(fileURLWithPath: path!)
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOfURL: soundURL)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
     }
-
-
+    
+    //MARK: Actions
+    @IBAction func numButtonPressed(sender: UIButton!) {
+        playBtnSound()
+        
+        runningNum += "\(sender.tag)"
+        ouputLabel.text = runningNum
+    }
+    
+    //MARK: Functions
+    func playBtnSound() {
+        btnSound.play()
+    }
 }
 
